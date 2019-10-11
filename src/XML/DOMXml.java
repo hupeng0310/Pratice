52376986e7edd108a8a568440d9deb509417a036
hupeng0310 <hupgwuty@163.com>
2019-10-11 16:32:50 +0800

    添加了Dom解析XML文件的demo

---
+++ src/XML/DOMXml.java
@@ -0,0 +1,41 @@
+package XML;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+
+public class DOMXml {
+    public static void main(String[] args){
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        File  file = new File("Resource/XML/mysqlConfig.xml");
+        try {
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+            Document document = documentBuilder.parse(file);
+            NodeList configureNodeList = document.getElementsByTagName("configure");
+            NodeList hostNodeList = document.getElementsByTagName("host");
+            NodeList userNodeList = document.getElementsByTagName("user");
+            NodeList passwordNodeList = document.getElementsByTagName("password");
+            NodeList databaseNodeList = document.getElementsByTagName("database");
+            for(int i = 0;i<configureNodeList.getLength();i++){
+                System.out.println("第"+(i+1)+"个配置文件信息");
+                System.out.println("Host:"+hostNodeList.item(i).getFirstChild().getNodeValue());
+                System.out.println("User"+userNodeList.item(i).getFirstChild().getNodeValue());
+                System.out.println("Password:"+passwordNodeList.item(i).getFirstChild().getNodeValue());
+                System.out.println("Database:"+databaseNodeList.item(i).getFirstChild().getNodeValue());
+            }
+
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
