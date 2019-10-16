27f36900910cda42c047532e4d3da576afb79596
hupeng0310 <hupgwuty@163.com>
2019-10-16 17:49:17 +0800

    JDBC代码Demo 封装了MysqlConnection对象获取Connection

---
+++ src/demo/JDBC/MysqlConnection.java
@@ -0,0 +1,38 @@
+package demo.JDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MysqlConnection {
+    private static final String className = "com.mysql.cj.jdbc.Driver";
+    private static final String urlHead = "jdbc:mysql://";
+    private static final String urlEnd = "?characterEncoding=utf8&useSSL=false&serverTimezone=UTC&rewriteBatchedStatements=true";
+    private static String address;
+    private static int port;
+    private static String user;
+    private static String password;
+    //放置一个静态的Connection对象，以提高性能
+    private static Connection connection;
+    private static String database;
+    //设置数据库的配置信息
+    private static void setConfig(String address,int port,String user,String password,String database){
+        MysqlConnection.address = address;
+        MysqlConnection.port = port;
+        MysqlConnection.user = user;
+        MysqlConnection.password = password;
+        MysqlConnection.database = database;
+    }
+    public static Connection getConnection() throws SQLException {
+        if(connection == null || connection.isClosed()){
+            connection = DriverManager.getConnection(urlHead+address + ":" + port +"/" +database +urlEnd,user,password);
+        }
+        return connection;
+    }
+    public static Connection getConnection(String address,int port,String user,String password,String database) throws SQLException {
+        setConfig(address, port, user, password, database);
+        return getConnection();
+    }
+    private MysqlConnection(){
+    }
+}
---
+++ src/demo/JDBC/Test.java
@@ -0,0 +1,16 @@
+package demo.JDBC;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Test {
+    public static void main(String[] args) throws SQLException {
+        MysqlConnection.getConnection("localhost",3306,"root","root","mysql");
+        Statement statement = MysqlConnection.getConnection().createStatement();
+        ResultSet resultSet = statement.executeQuery("select host from user");
+        while(resultSet.next()){
+            System.out.println(resultSet.getString(1));
+        }
+    }
+}
