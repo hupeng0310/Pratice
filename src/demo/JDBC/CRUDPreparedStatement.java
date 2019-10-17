7d0b5016929a648bea7d8e0a3c85c4c3c519c570
hupeng0310 <hupgwuty@163.com>
2019-10-17 19:07:10 +0800

    增加了使用PreparedStatement的CRUD示例

---
+++ src/demo/JDBC/CRUDPreparedStatement.java
@@ -0,0 +1,61 @@
+package demo.JDBC;
+
+import java.sql.*;
+
+public class CRUDPreparedStatement {
+    public static void createForMysql(){
+        try {
+            //该链接使用我自己封装的MysqlConnection对象获得，不要照抄
+            Connection connection = MysqlConnection.getConnection("localhost",3306,"root","root","project");
+            PreparedStatement preparedStatement = connection.prepareStatement("insert into user values(?,?)");
+            preparedStatement.setString(1,"hupeng");
+            preparedStatement.setString(2,"password");
+            int c = preparedStatement.executeUpdate();
+            System.out.println(c);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    };
+    //删
+    public static void deleteForMysql(){
+        try{
+            PreparedStatement preparedStatement = MysqlConnection.getConnection().prepareStatement("delete from user where account = ?");
+            preparedStatement.setString(1,"hupeng");
+            int d = preparedStatement.executeUpdate();
+            System.out.println(d);
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+    //改
+    public static void updateForMysql(){
+        try{
+            PreparedStatement preparedStatement = MysqlConnection.getConnection().prepareStatement("update user set password = 'hupeng' where account = ?");
+            preparedStatement.setString(1,"hupeng");
+            int u = preparedStatement.executeUpdate();
+            System.out.println(u);
+        }catch (SQLException e){
+
+        }
+    }
+    //查
+    public static void readForMysql() {
+        try {
+            PreparedStatement preparedStatement = MysqlConnection.getConnection().prepareStatement("select * from user");
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                System.out.println("account:" + resultSet.getString("account"));
+                System.out.println("password:" + resultSet.getString("password"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+        public static void main(String args[]){
+        createForMysql();
+        readForMysql();
+        updateForMysql();
+        deleteForMysql();
+    }
+}
--- src/demo/JDBC/MysqlConnection.java
+++ src/demo/JDBC/MysqlConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class MysqlConnection {
     private static final String className = "com.mysql.cj.jdbc.Driver";
     private static final String urlHead = "jdbc:mysql://";
-    private static final String urlEnd = "?characterEncoding=utf8&useSSL=false&serverTimezone=UTC&rewriteBatchedStatements=true";
+    private static final String urlEnd = "?characterEncoding=utf8&useSSL=false&serverTimezone=UTC&rewriteBatchedStatements=true&allowPublicKeyRetrieval=true";
     private static String address;
     private static int port;
     private static String user;
@@ -16,6 +16,13 @@ public class MysqlConnection {
     private static Connection connection;
     private static String database;
     //设置数据库的配置信息
+    static {
+        try {
+            Class.forName(className);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
     private static void setConfig(String address,int port,String user,String password,String database){
         MysqlConnection.address = address;
         MysqlConnection.port = port;
@@ -23,14 +30,13 @@ public class MysqlConnection {
         MysqlConnection.password = password;
         MysqlConnection.database = database;
     }
-    public static Connection getConnection() throws SQLException, ClassNotFoundException {
+    public static Connection getConnection() throws SQLException {
         if(connection == null || connection.isClosed()){
-            Class.forName(className);
             connection = DriverManager.getConnection(urlHead+address + ":" + port +"/" +database +urlEnd,user,password);
         }
         return connection;
     }
-    public static Connection getConnection(String address,int port,String user,String password,String database) throws SQLException, ClassNotFoundException {
+    public static Connection getConnection(String address,int port,String user,String password,String database) throws SQLException {
         setConfig(address, port, user, password, database);
         return getConnection();
     }
